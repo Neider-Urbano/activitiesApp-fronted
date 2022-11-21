db6bc2a16ce2385b555ffe9cb062c0bbd6dc3b5e
Neider Urbano <jjuly8julianur@gmail.com>
2022-11-21 15:20:28 -0500

    structure file project

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import './App.css';
+import { MyDiv } from './Style';
+
+function App() {
+  return (
+    <MyDiv>Hola</MyDiv>
+  );
+}
+
+export default App;
---
+++ src/Style.jsx
@@ -0,0 +1,8 @@
+import styled from 'styled-components';
+import tw from "twin.macro";
+
+export const MyDiv = styled.div`
+${tw`
+  bg-green-300 text-red-300
+`}
+`;
