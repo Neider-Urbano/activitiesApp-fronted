cc619eeaaeedff586608175b4af98204c4587fa0
Neider Urbano <jjuly8julianur@gmail.com>
2022-11-22 10:03:53 -0500

    files update login

--- src/containers/login/Login.jsx
+++ src/containers/login/Login.jsx
@@ -1,63 +1,38 @@
 import React from 'react'
 import { Main, DivContenedor,Form } from '../../style-components/login/Components'
+import {GroupLabel, ButtonAutenticar, ButtonLogin, ButtonRegister, DivForgotPassword} from '../../components/login/index'
+import { Player } from '@lottiefiles/react-lottie-player';
 
 const Login = () => {
   return (
     <Main>
-        <DivContenedor>
-            <h1 className="text-4xl font-medium">Login</h1>
-            <p className="text-slate-500">Hi, Welcome back 👋</p>
+        <div className=' flex justify-center items-center h-[100vh]'>
+            <DivContenedor>
+                    <h1 className="text-4xl font-medium">Login</h1>
+                    <p className="text-slate-500">Hi, Welcome back 👋</p>
 
-            <div className="my-5">
-                <button className="w-full text-center py-3 my-3 border flex space-x-2 items-center justify-center border-slate-200 rounded-lg text-slate-700 hover:border-slate-400 hover:text-slate-900 hover:shadow transition duration-150">
-                    <img src="https://www.svgrepo.com/show/355037/google.svg" className="w-6 h-6" alt="" /> <span>Login with Google</span>
-                </button>
-            </div>
+                    <ButtonAutenticar urlImg="https://www.svgrepo.com/show/355037/google.svg" textSpan="Login with Google"/>
+                    <ButtonAutenticar urlImg="https://www.svgrepo.com/show/355037/google.svg" textSpan="Login with Facebook"/>
 
-            <Form action="">
-                    <label htmlFor="email">
-                        <p className="font-medium text-slate-700 pb-2">Email address</p>
-                        <input id="email" name="email" type="email" className="w-full py-3 border border-slate-200 rounded-lg px-3 focus:outline-none focus:border-slate-500 hover:shadow" placeholder="Enter email address" />
-                    </label>
+                    <Form action="">
+                        <GroupLabel textP="Email address" type="email" placeholder="Enter email address"/>
+                        <GroupLabel textP="Password" type="password" placeholder="Enter your password"/>
 
-                    <label htmlFor="password">
-                        <p className="font-medium text-slate-700 pb-2">Password</p>
-                        <input id="password" name="password" type="password" className="w-full py-3 border border-slate-200 rounded-lg px-3 focus:outline-none focus:border-slate-500 hover:shadow" placeholder="Enter your password" />
-                    </label>
+                        <DivForgotPassword textA="Forgot Password?" textInput="Remenber me"/>
 
-                    <div className="flex flex-row justify-between">
-                        <div>
-                            <label htmlFor="remember" className="">
-                                <input type="checkbox" id="remember" className="w-4 h-4 border-slate-200 focus:bg-indigo-600" />
-                                Remember me
-                            </label>
-                        </div>
-                        <div>
-                            <a href="#" className="font-medium text-indigo-600">Forgot Password?</a>
-                        </div>
-                    </div>
-
-                    <button className="w-full py-3 font-medium text-white bg-indigo-600 hover:bg-indigo-500 rounded-lg border-indigo-500 hover:shadow inline-flex space-x-2 items-center justify-center">
-                        <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth="2">
-                            <path strokeLinecap="round" strokeLinejoin="round" d="M11 16l-4-4m0 0l4-4m-4 4h14m-5 4v1a3 3 0 01-3 3H6a3 3 0 01-3-3V7a3 3 0 013-3h7a3 3 0 013 3v1" />
-                        </svg>
-                        <span>Login</span>
-                    </button>
-                    
-                    <p className="text-center">
-                        Not registered yet? 
-                        <a href="#" className="text-indigo-600 font-medium inline-flex space-x-1 items-center">
-                            <span>Register now </span>
-                            <span>
-                                <svg xmlns="http://www.w3.org/2000/svg" className="h-4 w-4" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth="2">
-                                    <path strokeLinecap="round" strokeLinejoin="round" d="M10 6H6a2 2 0 00-2 2v10a2 2 0 002 2h10a2 2 0 002-2v-4M14 4h6m0 0v6m0-6L10 14" />
-                                </svg>
-                                </span>
-                        </a>
-                    </p>
-            </Form>
-
-        </DivContenedor>
+                        <ButtonLogin textSpan="Login" />
+                        <ButtonRegister textP="Not registered yet" textSpan="Register now"/>
+                    </Form>
+            </DivContenedor>
+            <div>
+                <Player
+                    src='https://assets7.lottiefiles.com/packages/lf20_tpnk0wok.json'
+                    className="player"
+                    loop
+                    autoplay
+                />
+            </div> 
+        </div>
     </Main>
   )
 }
