20475126effadf1fe8baf00ea6fcb5885fa45722
Neider Urbano <jjuly8julianur@gmail.com>
2022-11-29 22:45:59 -0500

    update alert error

--- src/containers/login/Login.jsx
+++ src/containers/login/Login.jsx
@@ -10,10 +10,10 @@ const initialState={
   "password":""
 }
 
-
 const Login = () => {
   const [dataUser,setDataUser]=useState(initialState)
   const [errors,setErrors]=useState("")
+  const [intentosFallidos,setIntentosFallidos]=useState(0)
   const notify = (error) => toast.error(error);
 
   const handleInput=(e)=>{
@@ -35,28 +35,41 @@ const Login = () => {
       Object.keys(dataUser).map((key)=>{
         if(errorEncontrado==="") errorEncontrado=validateInputs(key,dataUser[key])
       })
+
+      if((intentosFallidos+1)>4){
+        errorEncontrado="Has intentado ingresar varias veces"
+      }else{
+        e.target.disabled=false
+      }
+
       if(errorEncontrado===""){
         setDataUser(initialState)
       }else{
         setErrors(errorEncontrado);
+        toast.remove();
         notify(errorEncontrado)
+        setIntentosFallidos(intentosFallidos+1)
       }
-      e.target.disabled=false
+      
   }
 
   return (
     <Main>
       <div className='flex justify-center items-center h-[100vh]'>
+
         <DivContenedor>
+
           <h1 className="text-2xl font-medium">Login</h1>
           <p className="text-slate-500">Hi, Welcome to Activities app 👋</p>
 
           <div className=''> 
+            
             <ButtonAutenticar urlImg="https://www.svgrepo.com/show/355037/google.svg" 
               textSpan="Login with Google" nameAut="google"
             />
             <ButtonAutenticar urlImg="https://www.svgrepo.com/show/158427/facebook.svg" 
               textSpan="Login with Facebook" nameAut="facebook" />
+
           </div>
 
           <Form>
@@ -75,17 +88,21 @@ const Login = () => {
           </Form>
 
         </DivContenedor>
+
         <div className='hidden md:block ml-[20px] lg:ml-[50px] w-[30%]' key="loadImg">
           <Player
             src='https://assets7.lottiefiles.com/packages/lf20_tpnk0wok.json'
             className="player" loop autoplay
           />
         </div>
+
       </div>
+
       <Toaster 
         position="bottom-center"
         reverseOrder={false}
       />
+
     </Main>
   )
 }
