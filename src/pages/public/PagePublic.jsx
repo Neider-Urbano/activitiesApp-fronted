618d50f6ae1b0cdcdcd7178774819d3aa135e2b0
Neider08Urbano <80359162+Neider-Urbano@users.noreply.github.com>
2022-12-05 20:41:13 -0500

    Update PagePublic.jsx

--- src/pages/public/PagePublic.jsx
+++ src/pages/public/PagePublic.jsx
@@ -1,19 +1,34 @@
 import React from "react";
 import NavBar from "../../containers/menu/NavBar";
+
+
+
+
 import TextInicio from "../../components/Menu/TextInicio"
 
 const a=10;
-
+const b=20;
 
 const PagePublic=()=>{
     const b=50;
+    
+    
+    
+    
+    
+    
+    
+    
+    function a(){
+        console.log("hola") 
+    }
     return(
         <div class="h-[100vh] bg-[#EAF4FF] rounded-t-3xl">
-            <NavBar/>
             <TextInicio/>
                <div>
                     <p>Hola</p>        
                 </div>
+            <p>hola</p>
         </div>
     )
 };
